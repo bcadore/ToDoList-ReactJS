ed20dd7d5fd5c907f92bb85cbd18f0549797132a
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-06-26 21:56:51 -0300

    Adicionado componente de filtro.

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,7 +1,22 @@
+import FiltroCard from "../../components/FiltroCard"
+
+import * as S from './styles'
+
 const BarraLateral = () => (
-    <aside>
-        barra lateral
-    </aside>
+    <S.Aside>
+        <div>
+            <S.Campo type="text" placeholder="Buscar" />
+            <S.Filtros>
+                <FiltroCard />
+                <FiltroCard />
+                <FiltroCard />
+                <FiltroCard />
+                <FiltroCard />
+                <FiltroCard ativo />
+
+            </S.Filtros>
+        </div>
+    </S.Aside>
 )
 
 export default BarraLateral
---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Aside = styled.aside`
+    padding: 16px;
+    background-color: #EEEEEE;
+    height: 100vh;
+`
+
+export const Filtros = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 8px;
+    margin-top: 16px;
+`
+export const Campo = styled.input`
+    width: 100%;
+    padding: 8px;
+    font-weight: bold;
+    border-radius: 8px;
+    border-color: #666666;
+    color: #666666;
+    background-color: #FFFFFF;
+`
