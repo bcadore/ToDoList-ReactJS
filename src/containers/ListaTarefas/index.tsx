77c821e13c0e565ae6edbd10ebddff89dc76795d
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-06-26 23:45:08 -0300

    Criado estado para os botões.

--- src/containers/ListaTarefas/index.tsx
+++ src/containers/ListaTarefas/index.tsx
@@ -1,28 +1,44 @@
-import Tarefa from "../../components/Tarefa"
+import Tarefa from '../../components/Tarefa';
 
-import * as S from "./styles" 
+import * as S from './styles';
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Assistir a aula 3.',
+    prioridade: 'importante',
+    status: 'pendente',
+  },
+  {
+    titulo: 'Pagar conta de internet',
+    descricao: 'Baixar fatura e pagar a conta',
+    prioridade: 'urgente',
+    status: 'concluída',
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Realizar o treino do dia.',
+    prioridade: 'importante',
+    status: 'pendente',
+  },
+];
 
 const ListaTarefas = () => (
-    <S.Container>
-        <p></p>
-        <ul>
-            <li>
-                <Tarefa />
-            </li>
-            <li>
-                <Tarefa />
-            </li>
-            <li>
-                <Tarefa />
-            </li>
-            <li>
-                <Tarefa />
-            </li>
-            <li>
-                <Tarefa />
-            </li>
-        </ul>
-    </S.Container>
-)
+  <S.Container>
+    <p></p>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
+    </ul>
+  </S.Container>
+);
 
-export default ListaTarefas
+export default ListaTarefas;
