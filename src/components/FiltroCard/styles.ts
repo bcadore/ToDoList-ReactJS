d9b2ba07a30f183a0bf5ccdd786b2a8c14653da3
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-07-02 00:31:53 -0300

    Adicionado lógica para filtrar tarefas.

--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -1,23 +1,24 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { Props } from ".";
+type Props = {
+  ativo: boolean;
+};
 
-type PropsOmit = Omit<Props, 'contador' | 'legenda'>
-
-export const Card = styled.div<PropsOmit>`
-    padding: 8px;
-    border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#A1A1A1')};
-    border-radius: 8px;
-    background-color: ${(props) => (props.ativo ? '#FFF' : '#FCFCFC')};
-    color: ${(props) => (props.ativo ? '#1E90FF' : '#5E5E5E')};
-`
+export const Card = styled.div<Props>`
+  padding: 8px;
+  border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#A1A1A1')};
+  border-radius: 8px;
+  background-color: ${(props) => (props.ativo ? '#FFF' : '#FCFCFC')};
+  color: ${(props) => (props.ativo ? '#1E90FF' : '#5E5E5E')};
+  cursor: pointer;
+`;
 
 export const Contador = styled.span`
-    font-weight: bold;
-    font-size: 24px;
-    display: block
-`
+  font-weight: bold;
+  font-size: 24px;
+  display: block;
+`;
 
 export const Label = styled.span`
-    font-size: 14px;
-`
+  font-size: 14px;
+`;
