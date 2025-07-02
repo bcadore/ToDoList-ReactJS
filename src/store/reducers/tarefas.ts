cee76356fc6605c3e78675d6caf3c5f763eb82cc
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-07-01 21:14:30 -0300

    Adicionado feature para editar uma tarefa existente.

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -3,8 +3,8 @@ import Tarefa from '../../models/Tarefa';
 import * as enums from '../../utils/enums/Tarefa';
 
 type TarefasState = {
-  itens: Tarefa[]
-}
+  itens: Tarefa[];
+};
 
 const initialState: TarefasState = {
   itens: [
@@ -13,34 +13,45 @@ const initialState: TarefasState = {
       descricao: 'Estudar JavaScript',
       prioridade: enums.Prioridade.NORMAL,
       status: enums.Status.PENDENTE,
-      titulo: 'Estudar JavaScript'
+      titulo: 'Estudar JavaScript',
     },
     {
       id: 2,
       descricao: 'Estudar ReactJS',
       prioridade: enums.Prioridade.IMPORTANTE,
       status: enums.Status.PENDENTE,
-      titulo: 'Estudar ReactJS'
+      titulo: 'Estudar ReactJS',
     },
     {
       id: 3,
       descricao: 'Estudar TypeScript',
       prioridade: enums.Prioridade.URGENTE,
       status: enums.Status.CONCLUIDA,
-      titulo: 'Estudar TypeScript'
-    }
-  ]
-}
+      titulo: 'Estudar TypeScript',
+    },
+  ],
+};
 
 const tarefasSlice = createSlice({
   name: 'tarefas',
   initialState,
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
-      state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload);
+      state.itens = state.itens.filter(
+        (tarefa) => tarefa.id !== action.payload
+      );
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      );
+
+      if (indexTarefa >= 0) {
+        state.itens[indexTarefa] = action.payload;
+      }
     },
   },
 });
 
-export const { remover } = tarefasSlice.actions;
+export const { remover, editar } = tarefasSlice.actions;
 export default tarefasSlice.reducer;
