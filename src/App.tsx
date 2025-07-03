62a2a3405d0d7f1ac8f773bb94cfda3228e994c8
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-07-02 22:24:21 -0300

    Add react-router-dom para criação das rotas das páginas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { Provider } from "react-redux";
-import BarraLateral from "./containers/BarraLateral";
-import ListaTarefas from "./containers/ListaTarefas";
-import EstiloGlobal, { Container } from "./styles";
-import store from './store'
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import EstiloGlobal, { Container } from './styles';
+import store from './store';
+import Home from './pages/Home';
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova tarefa</h1>,
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral></BarraLateral>
-        <ListaTarefas></ListaTarefas>
+        <RouterProvider router={rotas}></RouterProvider>
       </Container>
     </Provider>
   );
